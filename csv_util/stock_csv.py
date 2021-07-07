840746de5e41553f7cfcd3d37d2a17b7545148f8
Diracckjy <45199852+Diracckjy@users.noreply.github.com>
2021-07-07 15:55:01 +0800

    Merge branch 'master' of D:\file\School\Spring2021\生产实习\python_data_analysis with conflicts.

--- csv_util/stock_csv.py
+++ csv_util/stock_csv.py
@@ -9,14 +9,34 @@ __author__ = "Shishao_Zhao"
 '''
 
 from pandas import DataFrame
+import pandas as pd
+import numpy as np
+
+# 读测试数据
+def load_test_data():
+    row_stock_data, row_stock_day_data = [], []
+    stock_file, stock_day_file = './stock.txt', './stock_day.txt'
+    tag = ['日期', '时间', '最高价', '最低价', '成交量']
+
+    with open(stock_file, mode='r', encoding='utf-8') as f:
+        for line in f:
+            row_stock_data.append(line.replace('\t', ' ').strip())
+
+    with open(stock_day_file, mode='r', encoding='utf-8') as f:
+        for line in f:
+            row_stock_day_data.append(line.replace('\t', ' ').strip())
+
+    # print(row_stock_data)
+    # print(row_stock_day_data)
+    return row_stock_data, row_stock_day_data
 
 
-# 预处理数据
-def pre_processing_stock(row_data=None):
-    if not row_data:
+# 预处理股票数据
+def pre_processing_stock(row_stock_data=None, row_day_data=None):
+    if not row_data or not row_day_data:
         return
 
-    all_data=[]
+    all_stock_data=[]
     for item in row_data:
         data = []
         data_list = item.split(' ')
@@ -28,26 +48,72 @@ def pre_processing_stock(row_data=None):
     return all_data
 
 
+# 从csv文件读入数据
+def load_from_csv(csv_file=None):
+    if not csv_file:
+        return
+    csv_data = pd.read_csv(csv_file, index_col=0)  #防止弹出警告
+    # print(csv_data)
+    df = pd.DataFrame(csv_data)
+    # print(type(df.values))   #numpy数组
+    # print(df.values.tolist())
+    return
+
+
 # 将数据存入csv文件
-def save_in_csv(all_data=None, tag=None):
-    if not all_data or not tag:
+def save_in_csv(all_data=None, tag=None, csv_file=None):
+    if not all_data or not tag or not csv_file:
         return
 
-    csv_path = './stock_dat.csv'
     df = DataFrame(data=all_data, columns=tag)
-    df.to_csv(path_or_buf=csv_path, encoding='utf-8')
+    df.to_csv(path_or_buf=csv_file, encoding='utf-8', index_label=False)
+
+
+# 预处理股票日数据
+def pre_processing_day_data(stock_info=None, row_data=None):
+    if not row_data or not stock_info:
+        return
+
+    all_data=[]
+    for item in row_data:
+        data = []
+        data_list = item.split(' ')
+        # 取出日期, 收盘价, 涨跌幅
+        for i in range(0, 3):
+            data.append(data_list[i])
+
+        all_data.append(data)
+
+    return all_data
+
+
+# 将股票日数据存入csv文件
+def save_day_data_in_csv(all_data=None, tag=None, csv_file=None):
+    if not all_data or not tag or not csv_file:
+        return
+
+    df = DataFrame(data=all_data, columns=tag)
+    df.to_csv(path_or_buf=csv_file, encoding='utf-8', index_label=False)
+
+
+# 从csv文件读出股票日数据
+def load_from_day_csv(file):
+
+    pass
 
 
 # 测试
 if __name__ == '__main__':
-    row_data = ["2021-06-29 21:34,124.50,124.15,8304",
-                "2021-06-29 21:35,124.83,124.43,10363",
-                "2021-06-29 21:36,124.91,124.27,8743",
-                "2021-06-29 21:37,124.90,124.33,5132",
-                "2021-06-29 21:38,125.60,124.88,12862",
-                "2021-06-29 21:39,125.67,125.30,10370",
-                "2021-06-29 21:40,125.84,125.66,5494"]
-    tag = ['日期', '时间', '最高价', '最低价', '成交量']
-    all_data = pre_processing_stock(row_data)
-    save_in_csv(all_data, tag)
-    pass
+    csv_day_file = './stock_day_data.csv'
+    stock_tag = ['股票序号', '股票代码', '股票名称', '相关链接', '市盈率(动态)', '市净率']
+    day_tag = ['日期', '收盘价', '涨跌幅']
+    stock_info = ['21', '688390', '固德威']
+
+    row_data, row_day_data = load_test_data()
+    day_data = pre_processing_day_data(stock_info, row_day_data)
+    print(day_data)
+
+    # all_data = pre_processing_stock(row_data)
+    # save_in_csv(all_data, tag, csv_file)
+    # load_from_csv(csv_file)
+
