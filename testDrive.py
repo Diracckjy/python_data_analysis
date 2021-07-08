f91f7d000890b0e998639caa167604d53b1cd3f8
Diracckjy <45199852+Diracckjy@users.noreply.github.com>
2021-07-08 09:35:07 +0800

    添加主驱动函数

---
+++ testDrive.py
@@ -0,0 +1,23 @@
+# 主测试函数
+if __name__ == '__main__':
+    # # 创建爬虫
+    # spider = stockDailySpider()
+
+    # # 爬取数据
+    # stock_name, row_day_data = spider.run()
+
+    # # 数据预处理
+    # day_data = pre_processing_data(row_day_data)
+
+    # # 将数据存入csv文件
+    # save_data_in_csv(day_data, day_tag, stock_name)
+
+    # # 将数据存入mysql
+
+    # # 从csv文件读出数据
+    # data = load_from_csv(stock_name + '.csv')
+
+    # # 生成日数据柱状、折线图
+
+    # # 生成数据饼图
+    pass
